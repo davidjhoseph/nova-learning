8e103718895e154a819e3e418c8a78473365260d
David Joseph <davidjoe781227@gmail.com>
2020-04-02 10:35:27 +0100

    first commit

---
+++ database/migrations/2020_04_01_192418_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->dateTime('published_at')->nullable();
+            $table->dateTime('published_until')->nullable();
+            $table->boolean('is_published')->default(false);
+            $table->string('category')->nullable();
+            
+
+
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
